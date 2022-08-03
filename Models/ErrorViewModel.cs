4e74857650cd5ca47f8a94960948ab201a1c47fe
appptqa <wilfried.nickl@anton-paar.com>
2022-08-03 14:35:09 +0200

    Initial check-in

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OktaJenkinsCI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
